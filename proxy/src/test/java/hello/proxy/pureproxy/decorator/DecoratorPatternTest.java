18120cddc4325ee337c7d441c0bf48f8e6c11c79
gos1004 <nnv711@gmail.com>
2022-02-02 11:27:32 +0900

    데코레이터 패턴 - 예제 코드3

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -21,4 +21,12 @@ public class DecoratorPatternTest {
 		client.execute();
 	}
 
+	@Test
+	void decorator2() {
+		Component realComponent = new RealComponent();
+		Component messageDecorator = new MessageDecorator(realComponent);
+		Component timeDecorator = new TimeDecorator(messageDecorator);
+		DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+		client.execute();
+	}
 }
