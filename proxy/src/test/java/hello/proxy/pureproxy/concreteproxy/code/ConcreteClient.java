bff8afb1ac8e74c26f3a00af519002c959652d14
gos1004 <nnv711@gmail.com>
2022-02-04 17:59:18 +0900

    구현 클래스 기반 프록시 - 적용

--- proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -2,7 +2,7 @@ package hello.proxy.pureproxy.concreteproxy.code;
 
 public class ConcreteClient {
 
-	private ConcreteLogic concreteLogic;
+	private ConcreteLogic concreteLogic;  //ConcreteLogic, TimeProxy 모두 주입 가능
 	public ConcreteClient(ConcreteLogic concreteLogic) {
 		this.concreteLogic = concreteLogic;
 	}
