03b81304304f43054045b45c85070e5bff285007
gos1004 <nnv711@gmail.com>
2022-02-06 23:06:00 +0900

    예제 코드 - 어드바이저

---
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,27 @@
+package hello.proxy.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AdvisorTest {
+
+	@Test
+	void advisorTest1() {
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+		proxyFactory.addAdvisor(advisor);
+		ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+	}
+}
